5b82eff3b7e703285b7884f720750aecec2b8220
hmatheus00 <hmatheus00@outlook.com>
2021-06-06 11:21:25 -0300

    Implementação de logica nos botoes e finalização do designer

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,16 @@ import './Button.css';
 
 export default function Button(props){
     return (
-        <button className='button'>{props.label}</button>
+        <button className=
+            {
+                `button 
+                ${props.operation? 'operation' :''}
+                ${props.double? 'double': ''}
+                ${props.triple? 'triple': ''}`
+            }
+            onClick={e=> props.click && props.click(props.label)}
+        >
+            {props.label}
+        </button>
     );
 }
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,34 +1,48 @@
 import "./Calculator.css";
 import React from "react";
 import { Component } from "react";
-import Button from '../components/Button/Button'
-import Display from '../components/Display/Display'
+import Button from "../components/Button/Button";
+import Display from "../components/Display/Display";
 
 export default class Calculator extends Component {
-  render() {
-    return (
-        <div className="calculator">
-            <Display value={100}/>
-            <Button label="AC"/>
-            <Button label="/"/>
-            <Button label="7"/>
-            <Button label="8"/>
-            <Button label="9"/>
-            <Button label="*"/>
-            <Button label="4"/>
-            <Button label="5"/>
-            <Button label="6"/>
-            <Button label="-"/>
-            <Button label="1"/>
-            <Button label="2"/>
-            <Button label="3"/>
-            <Button label="+"/>
-            <Button label="0"/>
-            <Button label="."/>
-            <Button label="="/>
-           
-        </div>
-    );
+  clearMemory() {
+    console.log("limpar");
+  }
 
+  setOperation(operation) {
+    console.log(operation);
+  }
+
+  addDigit(n) {
+    console.log(n);
+  }
+
+  render() {
+    
+    const addDigit = n => this.addDigit(n);
+    const setOperation = op => this.setOperation(op);
+
+    return (
+      <div className="calculator">
+        <Display value={100} />
+        <Button label="AC" click={() => this.clearMemory()} triple/>
+        <Button label="/" click={setOperation} operation/>
+        <Button label="7" click={addDigit}/>
+        <Button label="8" click={addDigit}/>
+        <Button label="9" click={addDigit}/>
+        <Button label="*" click={setOperation} operation/>
+        <Button label="4" click={addDigit}/>
+        <Button label="5" click={addDigit}/>
+        <Button label="6" click={addDigit}/>
+        <Button label="-" click={setOperation} operation/>
+        <Button label="1" click={addDigit}/>
+        <Button label="2" click={addDigit}/>
+        <Button label="3" click={addDigit}/>
+        <Button label="+" click={setOperation} operation/>
+        <Button label="0" click={addDigit} double/>
+        <Button label="." click={addDigit}/>
+        <Button label="=" click={setOperation} operation/>
+      </div>
+    );
   }
 }
