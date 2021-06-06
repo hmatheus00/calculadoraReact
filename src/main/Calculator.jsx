9c96062f7e91abacafb6fbf18bebb9bc38c1d6e9
hmatheus00 <hmatheus00@outlook.com>
2021-06-05 23:30:07 -0300

    Criação da carcaça da calculadora

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import "./Calculator.css";
+import React from "react";
+import { Component } from "react";
+
+export default class Calculator extends Component {
+  render() {
+    return (
+        <div className="calculator">
+
+        </div>
+    );
+
+  }
+}
