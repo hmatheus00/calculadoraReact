2bda7338f9186c64b36b2a41e295b445aba2f878
hmatheus00 <hmatheus00@outlook.com>
2021-06-05 23:51:30 -0300

    Adicao de botoes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './main/Calculator';
 import '../src/index.css';
+import Calculator from './main/Calculator';
+
 
 ReactDOM.render(
   <React.StrictMode>
